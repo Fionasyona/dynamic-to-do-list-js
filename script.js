4ce2f40371b24bc68d21e4dd26893e5698c3007a
Fiona Wambua <fionawambua20@gmail.com>
2025-06-11 16:27:53 +0300

    new commits

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (){
 
-    const addButton = document.getElementById("Add Task");
+    const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
 
