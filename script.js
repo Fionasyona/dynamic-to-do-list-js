b02f7beaaf23414431c1dfcc39d7c1cf7303ad43
Fiona Wambua <fionawambua20@gmail.com>
2025-06-11 16:25:47 +0300

    all new changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function (){
 
     const addButton = document.getElementById("Add Task");
-    const taskInput = document.getElementById(id="task-input");
-    const taskList = document.getElementById(id="task-list");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
 
     function addTask() {
 
